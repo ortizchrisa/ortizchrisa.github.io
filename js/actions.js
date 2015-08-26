3bf2ac2264bc565ee888913dab757ad7a9c9d59b
ortizchrisa <ortizchrisa@gmail.com>
2015-08-25 20:10:04 -0400

    Updated Sal's version
    Pushed Sal’s version to live environment. Used OGGs for fallbacks

--- js/actions.js
+++ js/actions.js
@@ -4,45 +4,106 @@
 			$(this).stop().fadeTo('fast',0.5);
 		},
 		
-        function(){
+		function(){
 			$(this).stop().fadeTo('slow',1);
 		});
 
-    $(".content img").click(
-        function(){
-            $("htmnl, body").stop().fadeTo("slow", 0.5);
-            
-        })
+	$(".content img").click(
+		function(){
+			$("htmnl, body").stop().fadeTo("slow", 0.5);
+			
+		})
 */
 /*	$('a[href*=#]:not([href=#])').click(
-        function(){
-    		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-            || location.hostname == this.hostname){
-                var target = $(this.hash);
-            
-                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-           
-           if (target.length) {
-                $('body').animate({
-                    scrollTop: target.offset().top
-                }, 1000);
-                return false;
-            }
-        }
-    });
+		function(){
+			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+			|| location.hostname == this.hostname){
+				var target = $(this.hash);
+			
+				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		   
+		   if (target.length) {
+				$('body').animate({
+					scrollTop: target.offset().top
+				}, 1000);
+				return false;
+			}
+		}
+	});
 });*/
 
+
 function init(){
-    console.log("actions.js loaded");
+	console.log("actions.js loaded");
 
-    for(var i = 0; i<thumbImg.length; i++){
-        thumbImg[i].addEventListener("click", thumbClick, false);
-        thumbImg[i].addEventListener("mouseover", thumbMouseOver, false);
-        thumbImg[i].addEventListener("mouseout", thumbMouseOut, false);
-    }
+	//	Hover effect
+	$('#gallery>div>a').each( function(){
+		this.addEventListener("mouseover", thumbMouseOver, false);
+	});
+	$('#gallery>div>a').each( function(){
+		this.addEventListener("mouseout", thumbMouseOut, false);
+	});
+
+	//	Assign class for Magnific to target
+	$('.thumbImg').parent().addClass("popup");
+
+	var href = document.location.href;
+	var pageName = href.substr(href.lastIndexOf('/') + 1);
+
+	if(pageName == 'index.html') {
+		$(".popup").magnificPopup({
+			type:"image",
+			verticalFit: true,
+			removalDelay: 150,
+			mainClass: "mfp-fade",
+			gallery:{
+				enabled:true,
+				tCounter: "<span class='mfp-counter'>%curr% of %total%</span>"
+			},
+			image:{titleSrc:'data-title'}
+		});
+	}
+	else if(pageName == 'dev_design.html') {
+		$(".video-popup").append(
+			"<div class='mfp-bottom-bar'>"+
+				"<div class='mfp-title'></div>"+
+				"<div class='mfp-counter'></div>"+
+			"</div>"
+		);
+
+		$(".popup").magnificPopup({
+			type:"inline",
+			removalDelay: 150,
+			mainClass: "mfp-fade",
+			//closeBtnInside: false,
+			gallery:{
+				enabled:true,
+				tCounter: "<span class='mfp-counter'>%curr% of %total%</span>"
+			},
+  			callbacks: {
+				markupParse: function(template, values, item) {
+					values.title = item.el.attr('data-title');
+				},
+				change: function(){
+					//	Autoplay!
+					$(this.content).find('video').get(0).currentTime = 0;
+					$(this.content).find('video').get(0).play();
+				}
+  			}
+		});
+
+		$(".video-popup").each( function(){
+			$(this).find('video').css('max-height', $(window).height()-80+'px');
+			$(this).find('video').css('max-width', $(window).width()-20+'px');
+		});
+		$(window).resize(function(){
+			$(".video-popup").each( function(){
+				$(this).find('video').css('max-height', $(window).height()-80+'px');
+				$(this).find('video').css('max-width', $(window).width()-20+'px');
+			});
+		});
+	}
 }
-
-var thumbImg = document.getElementsByClassName("thumbImg");
 
 var tt = TweenLite.to;
 var tf = TweenLite.from;
@@ -50,27 +111,27 @@ var ts = TweenLite.set;
 var td = TweenLite.delayedCall;
 
 function thumbClick(){
-    console.log("thumb clicked");
+	$(this).magnificPopup({type:'image'});
 }
 
 function thumbMouseOver(){
-    tt(this,.5,{opacity:.5});
+	tt(this,.5,{opacity:.5});
 }
 
 function thumbMouseOut(){
-    tt(this,.5,{opacity:1});
+	tt(this,.5,{opacity:1});
 }
 
 function containerExitFrame(){
-    console.log("poop out");
-    tt(gallery,1,{opacity:0});
-    tt(gallery,0,{display:"none",delay:1});
+	console.log("poop out");
+	tt(gallery,1,{opacity:0});
+	tt(gallery,0,{display:"none",delay:1});
 }
 
 function containerEnterFrame(){
-    console.log("poop in");
-    tt(gallery,0,{display:"inline",delay:1});
-    document.getElementById("gallery").style.left=960;
-    tt(gallery,1,{left:0,opacity:1});
+	console.log("poop in");
+	tt(gallery,0,{display:"inline",delay:1});
+	document.getElementById("gallery").style.left=960;
+	tt(gallery,1,{left:0,opacity:1});
 }
 
